87f33453609e70273aaf6e5b7f1029d3d01f32a5
Jazmine Miller <jazminedmiller@gmail.com>
2022-05-23 18:49:44 -0400

    added home page header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,24 @@
-//Header for Home Page?
-
+//Header for Home Page 
+  
 import React from 'react'
+import { Link } from 'react-router-dom'
+import ElectricBoltIcon from '@mui/icons-material/ElectricBolt';
 
 function Header() {
   return (
-    <div>Header</div>
+    <div className="header">
+      <div className="header-links">
+        <ElectricBoltIcon />
+        <Link to="#" id="logo-link">Every Volt</Link>
+        <Link to="#">Locations</Link>
+      </div>
+
+      <div className="header-links">
+        <Link to="#" id="signup-link">Sign Up</Link>
+        <Link to="#">Sign In</Link>
+      </div>
+
+    </div>
   )
 }
 
