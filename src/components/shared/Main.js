51a782ed9d28bc55f31bfafcdc2d3fc2d3c37f3e
hongb918 <hong.b918@gmail.com>
2022-05-25 21:32:12 -0500

    Editing API URL

--- src/components/shared/Main.js
+++ src/components/shared/Main.js
@@ -2,77 +2,66 @@
 import React, { useEffect, useState } from 'react'
 import Locations from './location/Locations'
 import { Routes, Route } from 'react-router-dom'
-const URL = "https://every-volt-back.herokuapp.com/"
-
 
 function Main() {
+  console.log(process.env)
+
+  const searchOptions = {
+    key: "3d636052-e9ab-4daa-82ec-8a16bec907bb",
+    limit: 5,
+    api: 'https://api.openchargemap.io/v3/poi',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    countrycode: 'US',
+    distance: 10,
+    distanceunit: 'miles',
+    lat: '47.6062',
+    lon: '122.3321'
+  }
+
   const [locations, setLocations] = useState(null)
 
-  //const querySearch
-
-  const getLocation = () => {
-    fetch(URL)
-      .then(res => res.json())
-      .then(data => {
+  const getLocations = () => {
+    const api = `${searchOptions.api}?key=${searchOptions.key}&countrycode=${searchOptions.countrycode}&maxresults=${searchOptions.limit}&lat=${searchOptions.lat}&lon=${searchOptions.lon}&distance=${searchOptions.distance}&distanceunit=${searchOptions.distanceunit}`
+    fetch(api)
+      .then((res) => res.json())
+      .then((data) => {
         setLocations(data)
         console.log(data)
+        // console.log(`${data[0].AddressInfo.Title}`)
       })
       .catch(console.error)
   }
 
-  const createLocation = async (location) => {
-    await fetch(URL, {
-      method: 'post',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(location)
-    })
-    getLocation()
-  }
+  useEffect(() => {
+    getLocations()
+  }, [])
 
-  const updateLocation = async (location, id) => {
-    await fetch(URL + id, {
-      method: 'put',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body:  JSON.stringify(location)
-    })
-    getLocation()
-  }
-
-  const deleteLocation = async (id) => {
-    await fetch(URL + id, {
-      method: 'delete'
-    })
-    getLocation()
-  }
-
-  useEffect(() => getLocation(), [])
-  
   if (!locations) {
-    return <h3>Loading</h3>
+    return (
+      <>
+        <h3>Loading</h3>
+      </>
+    )
   }
-  console.log(`The first location is ${locations[0].title}`)
+
 
   return (
-    <main>
+    <div>
+      {/*       <Locations
+        locations={locations} /> */}
       <Routes>
         <Route exact path='/locations'
           element={<Locations
             locations={locations}
-            createLocation= {createLocation}
           />} />
         <Route exact path='/locations/:id'
           element={<Locations
             locations={locations}
-            updateLocation={updateLocation} 
-            deleteLocation={deleteLocation}
-          />}
-            />
+          />} />
       </Routes>
-    </main>
+    </div>
   )
 }
 
