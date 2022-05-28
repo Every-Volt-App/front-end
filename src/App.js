d03d05eb02593ac84bb24ac02a6aacdb72afc40f
Larissa Vieira <lv23@nyu.edu>
2022-05-28 15:10:11 -0400

    User authentication and authorization complete

--- src/App.js
+++ src/App.js
@@ -1,35 +1,33 @@
-import Header from './components/Header';
-import Nav from './components/Nav';
-import Main from './components/shared/Main';
-import Footer from './components/Footer';
-import './App.css';
-import { useEffect } from 'react';
-import React, { useState } from 'react';
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Main from "./components/shared/Main";
+import Footer from "./components/Footer";
+import "./App.css";
+import { useEffect } from "react";
+import React, { useState } from "react";
 
 function App() {
-
   //reference to backend url
-  // const URL = "http://localhost:4000"
-  const URL = "https://every-volt-back.herokuapp.com"
+  // const URL = "http://localhost:4000";
+  // const URL = "https://every-volt-back.herokuapp.com";
 
-  const [data, setData] = useState()
+  // const [data, setData] = useState();
 
-  const getLink = () => {
-    fetch(`${URL}/locations`)
-      .then(res => res.json())
-      .then(data => {
-        setData(data)
-      },
-      )
-      .catch(console.error)
-  }
+  // const getLink = () => {
+  //   fetch(`${URL}/user/register`)
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       setData(data);
+  //     })
+  //     .catch(console.error);
+  // };
 
-  useEffect(() => getLink(), [])
-  console.log(data)
+  // useEffect(() => getLink(), []);
+  // console.log(data);
 
-  if (!data) {
-    return <h2>Loading</h2>
-  }
+  // if (!data) {
+  //   return <h2>Loading App</h2>;
+  // }
 
   return (
     <div className="App">
