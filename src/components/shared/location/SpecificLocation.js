c4e2c421d2842a948200ab392391d47107271b2d
yesitwiz <grisellerivera@outlook.com>
2022-05-30 14:10:32 -0400

    if statements for SpecLoc

--- src/components/shared/location/SpecificLocation.js
+++ src/components/shared/location/SpecificLocation.js
@@ -9,30 +9,26 @@ function SpecificLocation(props) {
   const { id} = useParams();
 
   const locations = props.locations
-console.log(locations)
-//find record inside loc array where id matches var id
+// console.log(locations)
 
 const thisLocation = locations.find(loc => loc.ID == id)
 console.log(thisLocation.AddressInfo.title)
-//may need to convert to INTs
 //filter give you everything, find gives you one
 
-// const { id:index } = useParams();
-// console.log(index)
 
   return (
     <li className="specCardContainer">
       <div className="list-container">
         <span>{thisLocation.AddressInfo.Title}</span> 
         <span className="addy"> Address: {thisLocation.AddressInfo.AddressLine1}</span>
-        <span className="addy">{thisLocation.AddressInfo.Town}</span>
-        <span className="addy">{thisLocation.AddressInfo.Postcode}</span>
-        <span className="stretch">{thisLocation.UsageCost}</span>
-        <span className="stretch">{thisLocation.AddressInfo.ContactTelephone1}</span>
-        <span className="stretch">Operational: {thisLocation.StatusType.IsOperational}</span>
-        <span className="stretch">{thisLocation.StatusType.IsUserSelectable}</span>
-        <span className="stretch">{thisLocation.StatusType.Title}</span>
-        <span className="stretch">{thisLocation.UsageType.IsPayAtLocation}</span>
+        <span className="addy"> {thisLocation.AddressInfo.Town}</span>
+        <span className="addy"> {thisLocation.AddressInfo.Postcode}</span>
+        <span className="stretch">Usage Cost: {thisLocation.UsageCost}</span>
+        <span className="stretch">Phone: {thisLocation.AddressInfo.ContactTelephone1}</span>
+        <span className="stretch">Operational: {thisLocation.StatusType.IsOperational ? "yes" : "no"}</span>
+        <span className="stretch">User Selectable: {thisLocation.StatusType.IsUserSelectable ? "yes" : "no"}</span>
+        <span className="stretch">Status Type: {thisLocation.StatusType.Title}</span>
+        <span className="stretch">Pay at location: {thisLocation.UsageType.IsPayAtLocation === "false" ? "no" : "yes" }</span>
         <span className="stretch">{thisLocation.UsageType.Title}</span>
       </div>
     </li>
@@ -40,3 +36,5 @@ console.log(thisLocation.AddressInfo.title)
 }
 
 export default SpecificLocation;
+
+
