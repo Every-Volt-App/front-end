702224c3e8e8bcf4c45506bb9401cab12aeb0e20
Larissa Vieira <lv23@nyu.edu>
2022-06-01 19:38:51 -0400

    Revert "updated URL to pull info from backend" Reverts change to wrong branch This reverts commit 391b32dc0d25a8ec62f94939627bfa819a70f89d.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,17 @@
 //Footer for home page?
 
-import React from "react";
+import React from 'react'
 
 function Footer() {
   return (
     <div>
-      {/* <i class="fab fa-github" style='font-size:30px'></i>
+      <i class="fab fa-github" style='font-size:30px'></i>
       <i class="fab fa-stack-overflow" style='font-size:30px'></i>
-      <i class="fab fa-github" style='font-size:30px'></i> */}
+      <i class="fab fa-github" style='font-size:30px'></i>
       <br></br>
-      <p>
-        Copyright © 2022 Valkerie-creators - Website Design by me (who else? :D)
-      </p>
+      <p>Copyright © 2022 Valkerie-creators - Website Design by me (who else? :D)</p>
     </div>
-  );
+  )
 }
 
-export default Footer;
+export default Footer
