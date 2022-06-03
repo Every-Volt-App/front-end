11a740063679bb79c0f8509ee62ee4ef599ad4e3
hongb918 <hong.b918@gmail.com>
2022-06-03 18:45:07 -0500

    1

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,15 @@
 //Footer for home page?
 import React from 'react'
-import "../"
+import "../App.css"
 
 
 function Footer() {
   return (
-    <div>
+    <div className='footer'>
 {/*       <i class="fab fa-github" style='font-size:30px'></i>
       <i class="fab fa-stack-overflow" style='font-size:30px'></i>
       <i class="fab fa-github" style='font-size:30px'></i> */}
-      <br></br>
-      <p>
-        Copyright © 2022 Valkerie-creators - Website Design by me (who else? :D)
-      </p>
+      <p>Copyright © 2022 Valkerie-creators - Website Design by me (who else? :D)</p>
     </div>
   );
 }
