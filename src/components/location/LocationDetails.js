4eb5ec9dee993a0917fb9374ca9c024ba71a7cc0
hongb918 <hong.b918@gmail.com>
2022-06-03 22:39:42 -0500

    Locations.js

---
+++ src/components/location/LocationDetails.js
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from "react";
+import { Link } from 'react-router-dom';
+// import Header from "../../Header";
+import StarRateIcon from '@mui/icons-material/StarRate';
+// import Paper from '@mui/material/Paper';
+import StarHalf from "@mui/icons-material/StarHalf";
+// import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+
+function LocationDetails(props) {
+  const { title, address, town, postcode, cost, id } = props
+  useEffect(() => {
+  }, []);
+  const [expanded, setExpanded] = useState(false)
+  return (
+    <div>
+      {/* <div className=".header-nav">
+            <Header />
+           
+        </div> */}
+      {/* <div className="location-hero"> */}
+        <h1 className="title">Location</h1>
+      {/* </div> */}
+{/*       <section className="location-info">
+        <div className="address-div">
+          <h1>Address</h1>
+          <h1>Phone Number</h1>
+        </div>
+        <div className="cost-div">
+          <h1>Cost</h1>
+          <h1>Availability</h1>
+        </div>
+      </section> */}
+      {/*         <section className="rating-div">
+          <div className="rating">
+            <StarRateIcon sx={{ fontSize: 50}}/>
+            <StarRateIcon sx={{ fontSize: 50}}/>
+            <StarRateIcon sx={{ fontSize: 50}}/>
+            <StarHalf sx={{ fontSize: 50}}/>
+          </div>
+        */}
+      {/*           <div className="boxes">
+            <div className="box">
+              <h2></h2>
+            </div>
+            <div className="box">
+            </div>
+            <div className="box">
+            </div>
+          </div> */}
+      {/* </section>  */}
+
+      <li className="cardContainer" onClick={() => { setExpanded(!expanded) }}>
+        <div className="cardPreview">
+          <span>{title}</span>
+        </div>
+        {expanded ?
+          (<div className="cardFull">
+            <span className='addy'> Address: {address}</span>
+            <span className='addy'>{town}</span>
+            <span className='addy'>{postcode}</span>
+            <span className='stretch'>{cost}</span>
+            <Link to={`/locations/${id}`} key={id}>see more</Link>
+          </div>
+          )
+          : null
+        }
+      </li>
+    </div>
+  )
+}
+export default LocationDetails
